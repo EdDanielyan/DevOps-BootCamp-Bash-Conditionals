f73811eca59373b7e62517ba6a8a0d82f5f7d221
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-05 13:44:41 +0400

    Update cond1.sh

--- cond1.sh
+++ cond1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-IFS=',' read -r -a array <<< "$1"
+IFS=', ' read -r -a array <<< "$1"
+sum=0
+for ((i=0;i<=${#array[@]};i++))
+do
+    if ((array[i]%2==0))
+    then
+        sum=$((array[i]+sum))
+    fi
+done
 
-#Write your code here
+echo "$sum"
