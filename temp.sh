36a5826c52af9850a6f7fbe65ea34ec2c7047145
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-06 17:42:25 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-# Place your code here
+#to convert centigrade, fahrenheit and kelvin
+
+#checking if any temperature scale is specified
+c=$(echo $1 | grep -c "c")
+f=$(echo $1 | grep -c "f")
+k=$(echo $1 | grep -c "k")
+
+
+#doing calculations
+if (( $c == "1" ));then
+   c=$(echo $1 | sed 's/c//g');
+   f=$(echo "scale=1;($c*(9/5))+32" | bc )
+   k=$(echo "scale=2;$c+273"|bc )
+   echo -e " $k k";
+elif (( $f == "1" ));then
+   f=$(echo $1 | sed 's/f//g');
+   c=$(echo "scale=1;(($f-32)*5)/9" | bc )
+   k=$(echo "scale=2;$c+273"|bc )
+   echo -e "$c c & $k k\e[0m\n"
+elif (( $k == "1" ));then
+   k=$(echo $1 | sed 's/k//g');
+   c=$(echo "scale=1;$k-273" | bc )
+   f=$(echo "scale=1;($c*(9/5))+32" | bc )
+   echo -e "$c c ";
+else
+   c=$(echo "scale=1;($1-32)*(5/9)" | bc )
+#   f=$(echo "scale=1;($1*(9/5))+32" | bc )
+   echo -e "$c c "
+fi
