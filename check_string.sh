c118407bec7f2ab065adbf059c6dce3a543f46eb
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-07 11:26:00 +0400

    Update check_string.sh

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # Place your code here
+
+char=$(echo "$1" | grep -o "[a-zA-Z]" | wc -l)
+special=$(echo "$1" | grep -o "[\!\@\#\$\%\^\&\(\)_\+\*]" | wc -l)
+numbers=$(echo "$1" | grep -o "[0-9]" | wc -l)
+
+# Output
+
+echo "Numbers $numbers: Symbols $special: Letters $char"
