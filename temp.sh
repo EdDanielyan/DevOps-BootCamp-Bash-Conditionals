8633dd8bd4b5a0c046b2832446f0989b1fd21da9
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-07 10:39:53 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# Place your code here
+
+if [[ ${1: -1} == 'C' ]]
+then
+  celsius=$(echo $1 | sed 's/C//g')
+  echo "$((celsius + 273))K"
+else
+  kelvin=$(echo $1 | sed 's/K//g')
+  echo "$((kelvin - 273))C"
+fi
